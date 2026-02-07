6b6cd752e11faff46e47ed9035ad07961d50010d
Fedor Osetrov <fiosetrova@gmail.com>
2026-02-07 10:38:07 +0000

    add mu test

--- modules/usage_sample.cpp
+++ modules/usage_sample.cpp
@@ -1,9 +1,36 @@
+// Copyright (c) 2016-2026 Antony Polukhin
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <iostream>
+#include <memory>
+#include <string>
 
 import boost.conversion;
 
+namespace {
+
+struct Base {
+    virtual ~Base() = default;
+    virtual std::string name() const {
+        return "base";
+    }
+};
+
+struct Derived : Base {
+    std::string name() const override {
+        return "derived";
+    }
+};
+
+}
+
 int main() {
-    std::cout << boost::implicit_cast<long>(42);
+    std::cerr << boost::implicit_cast<long>(42) << '\n';
+
+    std::unique_ptr<Base> base = std::make_unique<Derived>();
+    std::cerr << boost::polymorphic_downcast<Derived&>(*base).name() << '\n';
 
     return 0;
 }
---
+++ modules/usage_test_mu1.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2016-2026 Antony Polukhin
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <iostream>
+
+#include <boost/implicit_cast.hpp>
+
+long make_me_long(int x);
+
+int main() {
+    std::cout << make_me_long(42) << '\n';
+    std::cout << boost::implicit_cast<long>(42) << '\n';
+}
